0c4f98bac4c590d2fc760f636b3077944688160f
mgrinko <glukym@gmail.com>
2019-03-07 09:56:47 +0200

    Home Page was added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
+
+import HomePage from './HomePage';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +9,16 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Star Wars</h1>
+        <NavLink to="/" >Home</NavLink>
+        <NavLink to="/people">Home</NavLink>
+        <NavLink to="/films">Home</NavLink>
+
+        <section>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route render={() => <h1>Page not found</h1>}/>
+          </Switch>
+        </section>
       </div>
     );
   }
---
+++ src/HomePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className="HomePage">
+    <h1>Home page</h1>
+  </div>
+);
+
+export default HomePage;
